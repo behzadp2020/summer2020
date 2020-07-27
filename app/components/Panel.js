35b4aaf43f8cd35625f2d24dd709670eda79292b
Pouyanfar <behzad.pouyanfar@accenture.com>
2020-07-27 17:03:13 -0400

    Levels added to employees

--- app/components/Panel.js
+++ app/components/Panel.js
@@ -13,6 +13,7 @@ class Panel extends React.Component {
               <th>Full Name</th>
               <th>Location</th>
               <th>Skills</th>
+              <th>Level</th>
           </tr>
         </thead>
 
@@ -21,16 +22,19 @@ class Panel extends React.Component {
             <td>Smith, John</td>
             <td>New York Metro</td>
             <td>C++</td>
+            <td>11</td>
           </tr>
           <tr>
             <td>Smith, Ethan</td>
             <td>Seattle, WA</td>
             <td>SAS</td>
+            <td>7</td>
           </tr>
           <tr>
             <td>Smith, Emma</td>
             <td>Boston, MA</td>
             <td>Oracle Cloud</td>
+            <td>9</td>
           </tr>
         </tbody>
       </table>
